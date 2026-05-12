e926ffd2343fd561b2900be103fa8e48e43b6a05
M.Bilal Waris <bilalwaris543@gmail.com>
2026-05-12 16:17:30 +0500

    Added Bootstrap 5 CSS CDN

--- index.php
+++ index.php
@@ -11,6 +11,9 @@ include("./includes/config.php")
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sound Group</title>
     
+    <!-- Bootstrap 5 CSS CDN  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 <body>
     
