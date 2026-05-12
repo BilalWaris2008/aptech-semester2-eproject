17f45d44bb64a658e7d0518b3915fc6c8913d132
M.Bilal Waris <bilalwaris543@gmail.com>
2026-05-12 16:45:17 +0500

    Added Navbar

--- index.php
+++ index.php
@@ -15,9 +15,67 @@ include("./includes/config.php")
     <!-- Bootstrap 5 CSS CDN  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="./css/style.css">
+
 </head>
 
 <body>
+
+
+    <nav class="navbar navbar-expand-lg navbar-dark py-3 custom-navbar">
+        <div class="container">
+
+            <a class="navbar-brand" href="#">
+                SOUND GROUP
+            </a>
+
+            <button class="navbar-toggler"
+                data-bs-toggle="collapse"
+                data-bs-target="#menu">
+                <span class="navbar-toggler-icon"></span>
+
+            </button>
+
+            <div class="collapse navbar-collapse" id="menu">
+
+                <ul class="navbar-nav mx-auto">
+
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Music</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Videos</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Artists</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Albums</a>
+                    </li>
+
+                </ul>
+
+                <div>
+
+                    <a href="#" class="btn btn-outline-light">
+                        Login
+                    </a>
+
+                    <a href="#" class="btn btn-success">
+                        Register
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
 
 </body>
 
