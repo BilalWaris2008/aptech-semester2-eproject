72c32c58d78e5be7879e17b9a25f3f307332f47d
M.Bilal Waris <bilalwaris543@gmail.com>
2026-05-14 23:48:30 +0500

    Added Scrollbar & Selection Stylings

--- auth/register.php
+++ auth/register.php
@@ -49,7 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Spotify Style Register</title>
+    <title>Sound Group Register</title>
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
