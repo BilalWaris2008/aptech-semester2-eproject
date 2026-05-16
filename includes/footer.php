79e5bc4ee0495b17bfa85b3f4e789ca7d3f35133
M.Bilal Waris <bilalwaris543@gmail.com>
2026-05-16 16:36:06 +0500

    Added Header & Footer

---
+++ includes/footer.php
@@ -0,0 +1,135 @@
+    <!-- Footer  -->
+
+    <footer class="footer-section">
+
+        <div class="container">
+
+            <div class="row g-5">
+
+                <div class="col-lg-4">
+
+                    <div class="footer-about">
+
+                        <h2>SOUND GROUP</h2>
+                        <p>
+                            Sound Group is an entertainment platform for music and videos.
+                            Discover trending songs, artists, albums and watch the latest videos anytime.
+                        </p>
+
+                    </div>
+
+                </div>
+
+                <div class="col-lg-2 col-md-6">
+
+                    <div class="footer-links">
+
+                        <h4>Quick Links</h4>
+
+                        <ul>
+
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+
+                            <li>
+                                <a href="#">Music</a>
+                            </li>
+
+                            <li>
+                                <a href="#">Videos</a>
+                            </li>
+
+                            <li>
+                                <a href="#">Artists</a>
+                            </li>
+
+                        </ul>
+
+                    </div>
+
+                </div>
+
+                <!-- Categories -->
+
+                <div class="col-lg-2 col-md-6">
+
+                    <div class="footer-links">
+
+                        <h4>Genres</h4>
+
+                        <ul>
+
+                            <li>
+                                <a href="#">Pop</a>
+                            </li>
+
+                            <li>
+                                <a href="#">Hip Hop</a>
+                            </li>
+
+                            <li>
+                                <a href="#">Rock</a>
+                            </li>
+
+                            <li>
+                                <a href="#">Jazz</a>
+                            </li>
+
+                        </ul>
+
+                    </div>
+
+                </div>
+
+                <div class="col-lg-4">
+
+                    <div class="footer-social">
+
+                        <h4>Follow Us</h4>
+                        <p>Stay connected with us on social platforms.</p>
+
+                        <div class="social-icons">
+
+                            <a href="#">
+                                <i class="bi bi-facebook"></i>
+                            </a>
+
+                            <a href="#">
+                                <i class="bi bi-instagram"></i>
+                            </a>
+
+                            <a href="#">
+                                <i class="bi bi-twitter-x"></i>
+                            </a>
+
+                            <a href="#">
+                                <i class="bi bi-youtube"></i>
+                            </a>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="footer-bottom">
+
+                <p>© 2026 SOUND GROUP. All Rights Reserved.</p>
+
+            </div>
+
+        </div>
+
+    </footer>
+
+</body>
+
+<!-- Bootstrap 5 JS CDN -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,76 @@
+<?php
+
+include("./includes/config.php")
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sound Group</title>
+
+    <!-- Bootstrap 5 CSS CDN  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Bootstrap Icon CDN  -->
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.13.1/font/bootstrap-icons.min.css">
+
+    <!-- CSS Link -->
+    <link rel="stylesheet" href="./css/style.css">
+
+</head>
+
+<body>
+
+
+    <!-- Navbar Start -->
+
+    <nav class="navbar navbar-expand-lg navbar-dark py-3 custom-navbar">
+        <section class="container">
+
+            <a class="navbar-brand" href="#">
+                SOUND GROUP
+            </a>
+
+            <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#menu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <section class="collapse navbar-collapse" id="menu">
+
+                <ul class="navbar-nav mx-auto">
+
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Music</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Videos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Artists</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Albums</a>
+                    </li>
+
+                </ul>
+
+                <section>
+                    <a href="./auth/login.php" class="btn btn-outline-light">
+                        Login
+                    </a>
+                    <a href="./auth/register.php" class="btn btn-success register-btn">
+                        Register
+                    </a>
+                </section>
+            </section>
+        </section>
+    </nav>
