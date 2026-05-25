714c7aa53c772b0c5085e860bd2b09848735e2b5
M.Bilal Waris <bilalwaris543@gmail.com>
2026-05-26 00:55:56 +0500

    Added Protection in Home Pge

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: auth/login.php");
+    exit();
+}
+
+
 include("./includes/config.php");
 include("./includes/header.php");
 
